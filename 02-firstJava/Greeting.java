98edb82efe16af17919fc57bc7e7e59e627ddde8
TanyaSinha1012 <tsinha1998@gmail.com>
2023-08-15 13:36:30 +0530

    Java-2

---
+++ 02-firstJava/Greeting.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Greeting {
+
+	
+	public static void main(String args[]) {
+		System.out.println("Enter your name : ");
+		Scanner sc= new Scanner(System.in);
+		
+		String name= sc.nextLine();
+		System.out.println("Hello "+ name);
+	}
+}
